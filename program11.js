f4c5d923ad4279853a31fe8b5ef06cfbb1764848
UsmanSofa <usmansofawala70@gmail.com>
2026-07-29 18:05:44 +0530

    JavaScript Program added

---
+++ program11.js
@@ -0,0 +1,8 @@
+// Check Whether the number is even or odd 
+ let a = 2;
+
+ if(a/2==1){
+    console.log("The number is Even");
+ }else{
+    console.log("Number is odd");
+ }
---
+++ program12.js
@@ -0,0 +1,11 @@
+// Find the largest of two numbers. 
+
+let a = 10;
+let b = 15;
+
+if(a>b){
+    console.log("A is greater Number");
+}
+else{
+    console.log("B is Greater Number");
+}
